fae905988b82634bfbc259b9ce3dd1ac20e31ac2
noorqidam <noorqidam@gmail.com>
2019-09-03 15:00:11 +0700

    Tugas 12 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas12.js
@@ -0,0 +1,13 @@
+function tugas12(){
+  var buah = ["Pisang","Jeruk","Apel","Mangga"];
+  console.log(buah)
+
+  var buah2 = buah.pop()
+  console.log(buah)
+
+  var buah3 = buah.shift()
+  return buah
+
+
+}
+console.log(tugas12())
